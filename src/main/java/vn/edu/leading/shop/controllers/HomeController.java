7ff54049a96d32639fa539f2fea125ee9def0804
yen-pham <yenpham123456987@gmail.com>
2019-06-06 22:39:10 +0700

    show cart and product detail 5

--- src/main/java/vn/edu/leading/shop/controllers/HomeController.java
+++ src/main/java/vn/edu/leading/shop/controllers/HomeController.java
@@ -5,10 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import vn.edu.leading.shop.configs.IAuthenticationFacade;
+import vn.edu.leading.shop.models.ShipperModel;
 import vn.edu.leading.shop.models.UserModel;
 import vn.edu.leading.shop.services.CategoryService;
 import vn.edu.leading.shop.services.ProductService;
+import vn.edu.leading.shop.services.ShipperService;
 import vn.edu.leading.shop.services.UserService;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -18,12 +22,16 @@ public class HomeController {
     private final ProductService productService;
     private final UserService userService;
 
+    private final ShipperService shipperService;
+
+
     private final IAuthenticationFacade authentication;
 
-    public HomeController(CategoryService categoryService, ProductService productService, UserService userService, IAuthenticationFacade authentication) {
+    public HomeController(CategoryService categoryService, ProductService productService, UserService userService, ShipperService shipperService, IAuthenticationFacade authentication) {
         this.categoryService = categoryService;
         this.productService = productService;
         this.userService = userService;
+        this.shipperService = shipperService;
         this.authentication = authentication;
     }
 
@@ -36,13 +44,6 @@ public class HomeController {
         return "admin/home/shop";
     }
 
-//    @GetMapping("/shop/{id}/show")
-//    public String showProduct(@PathVariable Long id, RedirectAttributes redirect, Model model) {
-//       Optional<CategoryModel> categoryModel =categoryService.findById(id);
-//          model.addAttribute("products",categoryModel.get().getProducts());
-//          return  "admin/home/shop";
-//    }
-
     @GetMapping("/product-detail/{id}/{str}")
     public String product(@PathVariable("id") Long id, Model model, @PathVariable String str) {
         model.addAttribute("productModel", productService.findById(id));
@@ -52,12 +53,17 @@ public class HomeController {
     }
 
     @GetMapping("/add-to-cart")
-    public String addToCart() {
+    public String addToCart(Model model) {
+        List<ShipperModel> shipperModelList = shipperService.findAll();
+        UserModel userModel = userService.findByUsername(authentication.getAuthentication().getName()).orElse(new UserModel());
+        model.addAttribute("userModel", userModel);
+        model.addAttribute("shippers", shipperModelList);
         return "admin/home/cart";
     }
 
     @GetMapping("/checkout")
     public String checkout() {
+
         return "admin/home/checkout";
     }
 }
--- src/main/java/vn/edu/leading/shop/controllers/OrderController.java
+++ src/main/java/vn/edu/leading/shop/controllers/OrderController.java
@@ -8,37 +8,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import vn.edu.leading.shop.models.OrderModel;
-import vn.edu.leading.shop.services.CustomerService;
-import vn.edu.leading.shop.services.EmployeeService;
 import vn.edu.leading.shop.services.OrderService;
 import vn.edu.leading.shop.services.ShipperService;
 
 import javax.validation.Valid;
+import java.time.Instant;
 
 @Controller
 public class OrderController {
 
     private final OrderService orderService;
 
-    private final CustomerService customerService;
-
-    private final EmployeeService employeeService;
-
     private final ShipperService shipperService;
 
-
-    public OrderController(OrderService orderService, CustomerService customerService, EmployeeService employeeService, ShipperService shipperService) {
+    public OrderController(OrderService orderService, ShipperService shipperService) {
         this.orderService = orderService;
-        this.customerService = customerService;
-        this.employeeService = employeeService;
         this.shipperService = shipperService;
     }
+
 
     @GetMapping("/admin/orders")
     public String orders(Model model) {
         model.addAttribute("orders", orderService.findAll());
-        model.addAttribute("employees", employeeService.findAll());
-        model.addAttribute("customers", customerService.findAll());
         model.addAttribute("shippers", shipperService.findAll());
         return "admin/pages/orders";
     }
@@ -48,10 +39,11 @@ public class OrderController {
         if (result.hasErrors()) {
             return "admin/pages/orders";
         }
+        if(order.getId() == null){
+            order.setOrderDate(String.valueOf(Instant.now()));
+        }
         orderService.save(order);
         model.addAttribute("orders", orderService.findAll());
-        model.addAttribute("employees", employeeService.findAll());
-        model.addAttribute("customers", customerService.findAll());
         model.addAttribute("shippers", shipperService.findAll());
         redirect.addFlashAttribute("successMessage", "Saved order successfully!");
         return "admin/pages/orders";
@@ -62,15 +54,11 @@ public class OrderController {
         if (orderService.delete(id)) {
             redirect.addFlashAttribute("successMessage", "Deleted order successfully!");
             model.addAttribute("orders", orderService.findAll());
-            model.addAttribute("employees", employeeService.findAll());
-            model.addAttribute("customers", customerService.findAll());
             model.addAttribute("shippers", shipperService.findAll());
             return "admin/pages/orders";
         } else {
             redirect.addFlashAttribute("successMessage", "Not found!!!");
             model.addAttribute("orders", orderService.findAll());
-            model.addAttribute("employees", employeeService.findAll());
-            model.addAttribute("customers", customerService.findAll());
             model.addAttribute("shippers", shipperService.findAll());
             return "admin/pages/orders";
         }
